e33c270318a8108e4a7e700b3f8dfd6e92af968e
Sergey Shevtsov <sergey.shevtsov@xyicon.com>
2024-12-21 23:55:52 +0600

    Revit commands prep

--- Points4Revit.RVT/App.cs
+++ Points4Revit.RVT/App.cs
@@ -1,12 +1,87 @@
-﻿using System;
+﻿using Autodesk.Revit.UI;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Reflection;
+using System.Windows.Media.Imaging;
 
 namespace Points4Revit.RVT
 {
-    public class App
+    public class App : IExternalApplication
     {
+        public Result OnShutdown(UIControlledApplication application)
+        {
+            return Result.Succeeded;
+        }
+
+        public Result OnStartup(UIControlledApplication application)
+        {
+            if (!AddMenu(application, string.Empty))
+            {
+                return Result.Failed;
+            }
+            return Result.Succeeded;
+        }
+
+        private bool AddMenu(UIControlledApplication application, string tabname)
+        {
+            try
+            {
+                var assemblyPath = Assembly.GetExecutingAssembly().Location;
+                var resourceString = "pack://application:,,,/Points4Revit.RVT;component/Resources/Images/Ribbon/";
+
+                var rb = GetRibbonPanel(application, "Points4Revit", tabname);
+                rb.AddItem(
+                    new PushButtonData("cmdWallsCreation", "Walls\nCreation", assemblyPath, "Points4Revit.RVT.AppCommands.CmdWallsCreation")
+                    {
+                        ToolTip = "Create walls using external points",
+                        LargeImage = new BitmapImage(new Uri(string.Concat(resourceString, "32x32/WallsCreation.png")))
+                    });
+                rb.AddItem(
+                    new PushButtonData("cmdWallThickness", "Wall\nThickness", assemblyPath, "Points4Revit.RVT.AppCommands.CmdWallThickness")
+                    {
+                        ToolTip = "Change selecrted wall thickness",
+                        LargeImage = new BitmapImage(new Uri(string.Concat(resourceString, "32x32/WallThickness.png")))
+                    });
+            }
+            catch
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        private RibbonPanel GetRibbonPanel(UIControlledApplication application, string ribbonName, string tabName = null)
+        {
+            IList<RibbonPanel> ribbonPanels;
+            if (!string.IsNullOrEmpty(tabName))
+            {
+                try
+                {
+                    application.CreateRibbonTab(tabName);
+                }
+                catch { }
+                ribbonPanels = application.GetRibbonPanels(tabName);
+            }
+            else
+                ribbonPanels = application.GetRibbonPanels();
+
+            RibbonPanel ribbonPanel = null;
+            foreach (RibbonPanel rp in ribbonPanels)
+            {
+                if (rp.Name.Equals(ribbonName))
+                {
+                    ribbonPanel = rp;
+                    break;
+                }
+            }
+
+            if (ribbonPanel == null)
+                ribbonPanel = (string.IsNullOrEmpty(tabName)) ?
+                    application.CreateRibbonPanel(ribbonName) :
+                    application.CreateRibbonPanel(tabName, ribbonName);
+
+            return ribbonPanel;
+        }
     }
 }
---
+++ Points4Revit.RVT/AppCommands/CmdWallsCreation.cs
@@ -0,0 +1,13 @@
+﻿using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+
+namespace Points4Revit.RVT.AppCommands
+{
+    public class CmdWallsCreation : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            return Result.Succeeded;
+        }
+    }
+}
